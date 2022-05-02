fa8c852c696ff9b577965dc0d617f2e31c63dd2e
valter <valterlobo@gmail.com>
2022-05-02 01:23:55 -0300

    start - react tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Table from "./Table";
+import Form from "./Form";
+import Wiki from "./Wiki";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    data:[] , 
+    characters: [
+      {
+        name: "Charlie",
+        job: "DEV",
+        // the rest of the data
+      },
+      {
+        name: "MARIE",
+        job: "DOCTOR",
+        // the rest of the data
+      },
+    ],
+  };
+
+  removeCharacter = (index) => {
+    const { characters } = this.state;
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      }),
+    });
+  };
+
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] });
+  };
+
+  render() {
+    const { characters } = this.state;
+
+    return (
+      <div className="container">
+         <h1>Tutorial</h1>
+        
+        <Table
+          characterData={characters}
+          removeCharacter={this.removeCharacter}
+        />
+         <p>Add a character with a name and a job to the table.</p>
+        <Form handleSubmit={this.handleSubmit} />
+        <Wiki/>
+      </div>
+    );
+  }
 }
 
 export default App;
